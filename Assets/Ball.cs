2f76f28475c43932893df236774071ae82599c23
Nick Repetti <nickrepetti@gmail.com>
2017-12-19 11:15:28 -0800

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+  public float speed = 30;
+
+  void Start() {
+    // Initial Velocity
+    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    rb.velocity = Vector2.right * speed;
+  }
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    float xDir = 0;
+    string otherName = other.gameObject.name;
+    if (otherName == "PaddleLeft") {
+      xDir = 1;
+    } else if (otherName == "PaddleRight") {
+      xDir = -1;
+    } else {
+      return;
+    }
+
+    // Calculate new vector's y value based on where the ball hit
+    float ballY = transform.position.y;
+    float paddleY = other.transform.position.y;
+    float paddleHeight = other.collider.bounds.size.y;
+    Vector2 dir = new Vector2(xDir, (ballY - paddleY) / (paddleHeight / 2));
+
+    // Set Velocity with dir * speed
+    GetComponent<Rigidbody2D>().velocity = dir * speed;
+  }
+
+}
---
+++ Assets/PaddleMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour {
+
+  public float speed = 30;
+  public string axis = "Vertical";
+
+  void FixedUpdate() {
+    // Get user input for up/down
+    // If up/down, value will be 1/-1
+    float v = Input.GetAxisRaw(axis);
+
+    // Get the RigidBody attached to this Game Object
+    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    rb.velocity = new Vector2(0, v * speed);
+  }
+
+}
